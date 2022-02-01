6365aaaf229285334b51bfdd1c715c97bc9f04ab
ismailync <ismailyonca2001@gmail.com>
2022-02-01 21:50:53 +0300

    Blog ve Yorum düzenlemeleri

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,40 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID = 1,
+                    UserName = "İsmail"
+                },
+                 new UserComment
+                {
+                    ID = 2,
+                    UserName = "Hasan"
+                },
+                  new UserComment
+                {
+                    ID = 3,
+                    UserName = "Ali"
+                },
+                   new UserComment
+                {
+                    ID = 4,
+                    UserName = "Mert"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}
