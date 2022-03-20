b7256cf0c26dcb5094e506a5c64e201afd12ab82
ismailync <ismailyonca2001@gmail.com>
2022-03-20 14:10:32 +0300

    Giriş ve kayıt işlemeleri

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı boş olamaz");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail boş olamaz");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş olamaz");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("En az 2 karakter giriniz");
+            RuleFor(x => x.WriterName).MaximumLength(15).WithMessage("En fazla 15 karakter giriniz");
+        }
+    }
+}
--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,17 +11,36 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
+    
     public class RegisterController : Controller
     {
+
+        WriterManager wm = new WriterManager(new EfWriterRepository());
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
-        //[HttpPost]
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public IActionResult Index(Writer p)
+        {
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results = wv.Validate(p);
+            if (results.IsValid)
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "Deneme";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
     }
 }
